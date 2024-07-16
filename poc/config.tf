7d8e052c8a8fe6a598ae72bce28e4d4fc8cfec65
吉村直人 <ahowateto123@gmail.com>
2024-07-16 13:48:27 +0900

    terraform initまで

---
+++ poc/config.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "1.9.2"
+  backend "s3" {
+    bucket = "line-nandemo-tfstate"
+    key = "poc.terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.58.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
